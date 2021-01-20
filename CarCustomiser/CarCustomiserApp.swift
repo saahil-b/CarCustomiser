618173a30449ae4b84ebf036553a7f5797916858
Saahil <bahadur.s@etoncollege.org.uk>
2021-01-20 10:52:53 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Rakesh Bahadur on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
